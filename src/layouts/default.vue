5a6344ee762d7ae1f638eb63279a895d4ab3545c
David Salazar <david.asal@hotmail.com>
2025-07-20 20:21:58 -0400

    hide app footer

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,9 +3,14 @@
     <router-view />
   </v-main>
 
-  <AppFooter />
+  <AppFooter v-if="showFooter" />
 </template>
 
 <script setup>
-  //
+  import { useRoute } from 'vue-router'
+
+  const route = useRoute()
+
+  // Hide footer on specific paths (like '/client')
+  const showFooter = computed(() => route.path !== '/client')
 </script>
